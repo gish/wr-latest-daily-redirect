2744c081a9a433fb19fa09cf2514bd4bc02244bd
Erik Hedberg <erik@hedberg.at>
2019-12-11 10:59:51 +0100

    feat: support fitness subreddit

--- index.js
+++ index.js
@@ -11,22 +11,33 @@ dotenv.config();
 const CLIENT_ID = process.env.CLIENT_ID;
 const CLIENT_SECRET = process.env.CLIENT_SECRET;
 const PORT = process.env.PORT;
+const ORIGIN = process.env.ORIGIN;
 const USER_AGENT = `nodejs:${packagejson.name}:${packagejson.version} (by /u/murrtu)`;
 const PINTIFIER_KEY = process.env.PINTIFIER_KEY;
 const CACHE_AGE = 23 * 60 * 60 * 1e3;
+const SUBREDDITS = ['weightroom', 'fitness'];
 
 const createCache = () => {
-  let storage = {
-    timestampUtc: 0,
-    url: '',
-  };
+  let storage = SUBREDDITS.reduce(
+    (cache, subreddit) => ({
+      ...cache,
+      [subreddit]: {
+        timestampUtc: 0,
+        url: '',
+      },
+    }),
+    {},
+  );
 
   return {
-    get: () => storage,
-    update: url =>
+    get: subreddit => storage[subreddit],
+    update: subreddit => url =>
       (storage = {
-        timestampUtc: Date.now(),
-        url,
+        ...storage,
+        [subreddit]: {
+          timestampUtc: Date.now(),
+          url,
+        },
       }),
   };
 };
@@ -48,18 +59,19 @@ const getAccessToken = R.pipeP(
   R.path(['data', 'access_token']),
 );
 
-const getNewPosts = R.pipeP(
-  accessToken =>
-    axios({
-      method: 'get',
-      url: 'https://oauth.reddit.com/r/weightroom/',
-      headers: {
-        Authorization: 'bearer ' + accessToken,
-        'User-Agent': USER_AGENT,
-      },
-    }),
-  R.path(['data', 'data', 'children']),
-);
+const getNewPosts = subreddit =>
+  R.pipeP(
+    accessToken =>
+      axios({
+        method: 'get',
+        url: `https://oauth.reddit.com/r/${subreddit}/`,
+        headers: {
+          Authorization: 'bearer ' + accessToken,
+          'User-Agent': USER_AGENT,
+        },
+      }),
+    R.path(['data', 'data', 'children']),
+  );
 
 const findLatestDaily = R.pipe(
   R.filter(
@@ -71,17 +83,17 @@ const findLatestDaily = R.pipe(
   R.head,
 );
 
-const addDailyToCache = async (clientId, clientSecret) => {
+const addDailyToCache = subreddit => async (clientId, clientSecret) => {
   try {
     R.pipeP(
       await getAccessToken,
-      await getNewPosts,
+      await getNewPosts(subreddit),
       findLatestDaily,
       R.path(['data', 'url']),
       R.ifElse(
         R.equals(getDailyFromCache(cache.get())),
         R.identity,
-        cache.update,
+        cache.update(subreddit),
       ),
     )(clientId, clientSecret);
   } catch (e) {
@@ -90,26 +102,37 @@ const addDailyToCache = async (clientId, clientSecret) => {
 };
 
 const getDailyFromCache = R.path(['url']);
+const getTimestampfromCache = R.path(['timestampUtc']);
 
-const logVisit = (dailyUrl, userAgent) =>
+const logVisit = (subreddit, dailyUrl, userAgent) =>
   axios({
     method: 'GET',
     url: 'https://pintifier.herokuapp.com/api/v1/notification',
     params: {
       key: PINTIFIER_KEY,
       domain: USER_AGENT,
-      payload: JSON.stringify({url: dailyUrl, ua: userAgent}),
+      payload: JSON.stringify({subreddit, url: dailyUrl, ua: userAgent}),
     },
   });
 
-setInterval(() => addDailyToCache(CLIENT_ID, CLIENT_SECRET), 1 * 60 * 1e3);
-addDailyToCache(CLIENT_ID, CLIENT_SECRET);
+SUBREDDITS.map(subreddit => {
+  setInterval(
+    () => addDailyToCache(subreddit)(CLIENT_ID, CLIENT_SECRET),
+    1 * 60 * 1e3,
+  );
+  addDailyToCache(subreddit)(CLIENT_ID, CLIENT_SECRET);
+});
 
-app.get('/', (req, res) => {
+app.get('/r/:subreddit', (req, res) => {
   try {
-    const daily = getDailyFromCache(cache.get());
-    const cacheTimestamp = R.path(['timestampUtc'], cache.get());
-    logVisit(daily, req.get('User-Agent'));
+    const subreddit = req.params.subreddit;
+    if (!SUBREDDITS.includes(subreddit)) {
+      throw new Error(`Subreddit /r/${subreddit} not supported`);
+    }
+    const cachedSubreddit = cache.get(subreddit);
+    const daily = getDailyFromCache(cachedSubreddit);
+    const cacheTimestamp = getTimestampfromCache(cachedSubreddit);
+    logVisit(subreddit, daily, req.get('User-Agent'));
     res
       .append(
         'Cache-Control',
@@ -128,4 +151,12 @@ app.get('/', (req, res) => {
       .end();
   }
 });
+
+app.get('/', (_, res) => {
+  res
+    .status(302)
+    .set('Location', `${ORIGIN}:${PORT}/r/weightroom`)
+    .end();
+});
+
 app.listen(PORT);
